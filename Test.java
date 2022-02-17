56410718ce1da688f4ab383ba0ccfec4bd13ce39
Srujan Kumar <srujan.naligili@gmail.com>
2022-02-17 11:17:50 +0530

    This is my first check-in

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to testing masters automation demo");
+		
+		//System.out.println("Welcome to testing masters automation demo using java with selenium");
+
+	}
+
+}
